4edfe79a77517d770271e9c5e41ce7cd31c3b0ee
John Doe <john.doe@contoso.com>
2022-01-28 13:05:20 -0600

    minor

--- src/MyFirstAzureFunction/HelloWorldHttpTrigger.cs
+++ src/MyFirstAzureFunction/HelloWorldHttpTrigger.cs
@@ -6,7 +6,7 @@ using Microsoft.Azure.WebJobs;
 using Microsoft.Azure.WebJobs.Extensions.Http;
 using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.Logging;
-
+ 
 namespace AzureFunctionsUniversity.Demo
 {
     public static class HelloWorldHttpTrigger
